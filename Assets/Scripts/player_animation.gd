cbbc701c76687d70d3252e4c2f145e07641dd497
Jia Le <seahjiale@gmail.com>
2026-05-12 22:44:29 +0800

    initial commit

---
+++ Assets/Scripts/player_animation.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var player_movement : PlayerMovement
+@export var animation_player : AnimationPlayer
+@export var sprite : Sprite2D
+
+func _process(delta):
+	# direction of player
+	if player_movement.direction == 1:
+		sprite.flip_h = false
+	elif player_movement.direction == -1:
+		sprite.flip_h = true
+	
+	# running animation
+	if abs(player_movement.velocity.x) > 0:
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+		
+	# jump animation
+	if player_movement.velocity.y < 0:
+		animation_player.play("jump")
+	elif player_movement.velocity.y > 0:
+		animation_player.play("fall")
