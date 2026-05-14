7eac408f51a3608e34d1343ee7d4e29007a5d008
Jia Le <seahjiale@gmail.com>
2026-05-14 13:45:00 +0800

    fixed transition scene layer

--- Assets/Scripts/back_button.gd
+++ Assets/Scripts/back_button.gd
@@ -9,4 +9,4 @@ func _on_mouse_exited():
 	icon.position.y -= 5
 
 func _pressed():
-	get_tree().change_scene_to_file("res://Assets/Scenes/MainMenu/main_menu.tscn")
+	SceneTransition.transition_to("res://Assets/Scenes/MainMenu/main_menu.tscn")
--- Assets/Scripts/settings_button.gd
+++ Assets/Scripts/settings_button.gd
@@ -7,6 +7,3 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	label.position.y -= 5
-
-func _pressed():
-	get_tree().change_scene_to_file("res://Assets/Scenes/MainMenu/settings_menu.tscn")
