3819948d7668b543d262dd6ece0f166af658c3f6
yangjieloh <yangjieloh@gmail.com>
2026-06-23 22:55:10 +0800

    Area 3 complete and skeletonEnemy

--- Assets/Characters/SkeletonEnemy/Scripts/skeleton_enemy.gd
+++ Assets/Characters/SkeletonEnemy/Scripts/skeleton_enemy.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-@onready var sprite = $MushroomManEnemySprite
+@onready var sprite = $SkeletonSprite
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var animation_player = $AnimationPlayer
 var direction : Vector2
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var is_dead: bool = false
 var can_move: bool = false:
 	set(value):
 		can_move = value
@@ -25,29 +26,37 @@ func _process(_delta):
 		return
 	direction = player.global_position - global_position
 	if direction.x < 0:
-		sprite.flip_h = false
-	else:
 		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 
 func _physics_process(delta):
 	if not can_move:
 		return
-	if not is_on_floor():
-		velocity.y += get_gravity().y * delta
-	velocity.x = direction.normalized().x * 40
+	$RayCast2D.position.x = abs($RayCast2D.position.x) * (-1 if sprite.flip_h else 1)
+	var edge_ahead = $RayCast2D.is_colliding()
+	if edge_ahead:
+		velocity.x = direction.normalized().x * 40
+	else:
+		velocity.x = 0
 	move_and_slide()
 
 func take_damage(amount: int):
+	if is_dead:
+		return
 	health -= amount
 	if health > 0:
 		animation_player.play("Hurt")
 		await animation_player.animation_finished
-		find_child("FiniteStateMachine").change_state("Run")
-		
+		if not is_dead:
+			find_child("FiniteStateMachine").change_state("Walk")
 	else:
 		_die()
 
 func _die() -> void:
+	if is_dead:
+		return
+	is_dead = true
 	ExpManager.gain_xp(40)
 	velocity = Vector2.ZERO
 	set_physics_process(false)
